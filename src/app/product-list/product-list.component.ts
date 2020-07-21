b692ad00df67dda1606435b12ed4db1554c05183
up1 <somkiat.p@gmail.com>
2020-07-21 21:36:10 +0700

    Working with pipe

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -12,9 +12,9 @@ export class ProductListComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {
-    const p1 = new Product('01', 'Name 01', 100.0);
-    const p2 = new Product('02', 'Name 02', 200.5);
-    const p3 = new Product('03', 'Name 03', 300.75);
+    const p1 = new Product('01', 'Name 01', 1000.0);
+    const p2 = new Product('02', 'Name 02', 2000.5);
+    const p3 = new Product('03', 'Name 03', 3000.75);
     this.products.push(p1, p2, p3);
   }
 }
