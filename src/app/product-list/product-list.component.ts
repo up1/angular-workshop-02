ba64f8c1f74a8dc6c786e6b2026ab3caac3c0f23
Ratthawan Sermsuwannasuk <ratthawanser@mathregit.group>
2020-07-21 17:51:24 +0700

    decimal

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -4,17 +4,21 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css'],
+  styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
+
   products: Product[] = [];
 
-  constructor() {}
+  constructor() {
+
+  }
 
   ngOnInit(): void {
-    const p1 = new Product('01', 'Name 01', 100.0);
-    const p2 = new Product('02', 'Name 02', 200.5);
-    const p3 = new Product('03', 'Name 03', 300.75);
+    const p1 = new Product('aaa', '01', 'Name01', 50.00, true, 10.0);
+    const p2 = new Product('bbb', '02', 'Name02', 150.00, true, 8.0);
+    const p3 = new Product('ccc', '03', 'Name03', 60.00, true, 9.4);
     this.products.push(p1, p2, p3);
   }
+
 }
