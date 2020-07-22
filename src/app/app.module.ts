8b06546197fec6fc4b69f2a1ba6fadb66927d3fe
Ratthawan Sermsuwannasuk <ratthawanser@mathregit.group>
2020-07-22 10:04:30 +0700

    ws6

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { ProductListComponent } from './product-list/product-list.component';
+import { ProductNamePipe } from './product-name.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    ProductListComponent
+    ProductListComponent,
+    ProductNamePipe
   ],
   imports: [
     BrowserModule,
--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -15,9 +15,9 @@ export class ProductListComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    const p1 = new Product('aaa', '01', 'Name01', 50.00, true, 10.0);
-    const p2 = new Product('bbb', '02', 'Name02', 150.00, true, 8.0);
-    const p3 = new Product('ccc', '03', 'Name03', 60.00, true, 9.4);
+    const p1 = new Product('aaa', '01', 'Name 01', 50.25, true, 10);
+    const p2 = new Product('bbb', '02', 'Name 02', 150.00, true, 8);
+    const p3 = new Product('ccc', '03', 'Name 03', 60.255, true, 9.4);
     this.products.push(p1, p2, p3);
   }
 
