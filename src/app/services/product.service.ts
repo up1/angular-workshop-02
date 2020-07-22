433ea0192bc977dd9b52da5125f05bf8a4eb0e6f
Natthawit Janjaroen <natthawit.jan@gmail.com>
2020-07-22 15:09:51 +0700

    change to dummy api

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -3,7 +3,7 @@ import { Product } from '../model/product';
 import { HttpClient } from '@angular/common/http';
 import DefaultProducts from '../initData';
 
-import { Observable, throwError } from 'rxjs';
+import { Observable, throwError, of } from 'rxjs';
 import { catchError, retry } from 'rxjs/operators';
 @Injectable({
   providedIn: 'root',
@@ -12,9 +12,36 @@ export class ProductService {
   constructor(private http: HttpClient) {}
 
   getAllProducts(): Observable<Product[]> {
-    return this.http.get<Product[]>('http://165.22.255.58:3000/products', {
-      observe: 'body',
-      responseType: 'json',
-    });
+    const dummy: Product[] = [
+      {
+        code: 'AA 0001',
+        name: 'Product name 1',
+        price: 100,
+        rating: 5,
+        available: false,
+        imageUrl:
+          'https://th-live-01.slatic.net/original/795870113fb003d6e781310596480060.jpg',
+      },
+      {
+        code: 'BB 0002',
+        name: 'Product name 2',
+        price: 2000,
+        rating: 2,
+        available: true,
+        imageUrl:
+          'https://th-live-01.slatic.net/original/795870113fb003d6e781310596480060.jpg',
+      },
+      {
+        code: 'BB 0003',
+        name: 'Product name 3',
+        price: 2000,
+        rating: 3.5,
+        available: true,
+        imageUrl:
+          'https://th-live-01.slatic.net/original/795870113fb003d6e781310596480060.jpg',
+      },
+    ];
+
+    return of(dummy);
   }
 }
